03f947dc692e8a982d690d59cdd6bfde1622eb5b
Anshuman-Singh-8578 <black.gaming8578@gmail.com>
2025-10-27 10:30:16 +0530

    fixing bugs

--- app.py
+++ app.py
@@ -506,7 +506,7 @@ def get_weather(city):
                 "feels_like": data["main"]["feels_like"],
                 "humidity": data["main"]["humidity"],
                 "description": data["weather"][0]["description"],
-                "wind_speed": data["wind"]["speed"])
+                "wind_speed": data["wind"]["speed"]}
         else:
             return none
 
